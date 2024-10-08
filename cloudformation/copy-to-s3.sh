7a0e6f9c5e4b8fc3e2b60fcbd7f04fa3f88fe935
Marko (ServerlessLife) <marko@serverlesslife.com>
2024-10-08 06:58:43 +0200

    WIP

--- cloudformation/copy-to-s3.sh
+++ cloudformation/copy-to-s3.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-# Define the export name used in the CloudFormation stack output
-export_name="LoggerExtensionBucketName" # Adjust this if the actual export name differs
+# Path to the Lambda layer zip file
+file_path="./dist/extension.zip"
 
-# Fetch the S3 bucket name using the defined export name
+# Compute MD5 hash of the file content
+hash_value=$(md5sum $file_path | awk '{ print $1 }')
+
+# Create a new filename by appending the hash value
+new_file_name="extension-${hash_value}.zip"
+
+# Copy the file to a new name to include the hash
+cp $file_path "./dist/$new_file_name"
+
+# Retrieve the bucket name using CloudFormation export
+export_name="LoggerExtensionBucketName"
 bucket_name=$(aws cloudformation list-exports --query "Exports[?Name=='$export_name'].Value" --output text)
 
 # Check if the bucket name was retrieved
@@ -14,10 +24,9 @@ else
     echo "Using bucket name: $bucket_name"
 fi
 
-# Path to the Lambda layer zip file
-file_path="./dist/extension.zip"
+# Upload the new zip file to the S3 bucket
+aws s3 cp "./dist/$new_file_name" s3://$bucket_name/
 
-# Upload the zip file to the S3 bucket
-aws s3 cp $file_path s3://$bucket_name/
+echo "File $new_file_name uploaded successfully to $bucket_name"
 
-echo "File uploaded successfully to $bucket_name"
+aws cloudformation deploy --stack-name lambda-logger-extension --template-file cloudformation/layer.yaml  --parameter-overrides S3Key=extension-${hash_value}.zip \
--- src/interceptor.ts
+++ src/interceptor.ts
@@ -1,25 +1,30 @@
-const ORIGINAL_HANDLER_KEY = 'ORIGINAL_HANDLER';
+// @ts-ignore
+import { load } from "./aws/UserFunction";
+
+const ORIGINAL_HANDLER_KEY = "ORIGINAL_HANDLER";
+
+console.log("Original handler: ", process.env[ORIGINAL_HANDLER_KEY]);
 
 export async function handler(event: any, context: any) {
-  const regularHandler = async () => {
-    const handler = await getOriginalHandler();
-    return await handler(event, context);
-  };
+  console.log("INPUT EVENT: ", JSON.stringify(event, null, 2));
 
-  const regularHandlerPromise = regularHandler();
+  const handler = await getOriginalHandler();
 
-  const response = await regularHandlerPromise;
+  console.log("HANDLER: ", handler.toString());
+
+  const response = await handler(event, context);
+
+  console.log("RESPONSE: ", JSON.stringify(response, null, 2));
   return response;
 }
 
 async function getOriginalHandler(): Promise<any> {
-  // @ts-ignore
-  const { load } = await import('./aws/UserFunction');
+  console.log("LAMBDA_TASK_ROOT: ", process.env.LAMBDA_TASK_ROOT);
 
   if (process.env[ORIGINAL_HANDLER_KEY] === undefined)
-    throw Error('Missing original handler');
+    throw Error("Missing original handler");
   return load(
     process.env.LAMBDA_TASK_ROOT!,
-    process.env[ORIGINAL_HANDLER_KEY],
+    process.env[ORIGINAL_HANDLER_KEY]
   ) as Promise<any>;
 }
